9a4fe74f9c2a26f53578f38ff7dcb8ccebdafa89
jcuzmar <739043+jomikuza@users.noreply.github.com>
2023-05-12 11:47:46 -0400

    fix a Typo and change branch name to main

--- deck.go
+++ deck.go
@@ -9,7 +9,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diaomonds", "Hearts", "Clubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
