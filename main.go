7839c643c92bcb59208d9d13e9dadcd281ab7dae
jcuzmar <739043+jomikuza@users.noreply.github.com>
2023-05-16 08:23:05 -0400

    checking toString fn

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeck()
 
-	hand, remaininCards := deal(cards, 5)
+	//hand, remaininCards := deal(cards, 5)
+	//hand.Println()
+	//remaininCards.Println()
 
-	hand.Println()
-	remaininCards.Println()
+	fmt.Println(cards.toString())
 }
