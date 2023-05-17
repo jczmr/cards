d00e8fd999bad1fb697105369bec2bea6f64a6ef
jcuzmar <739043+jomikuza@users.noreply.github.com>
2023-05-16 22:45:26 -0400

    Adding several fn en deck.go

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
-
 func main() {
 
+	/*
+		cards := newDeck()
+
+		//hand, remaininCards := deal(cards, 5)
+		//hand.Println()
+		//remaininCards.Println()
+
+		fmt.Println(cards.toString())
+		cards.saveToFile("my_cards")
+	*/
+
+	/*
+		cards := newDeckFromFile("my_cards")
+		cards.Println()
+	*/
+
 	cards := newDeck()
+	cards.shuffle()
+	cards.Println()
 
-	//hand, remaininCards := deal(cards, 5)
-	//hand.Println()
-	//remaininCards.Println()
-
-	fmt.Println(cards.toString())
 }
